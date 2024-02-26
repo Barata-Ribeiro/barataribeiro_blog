4c9d9b17fca44987f2741540278598c84492d9de
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-02-26 10:22:44 -0300

    refactor: modify login flow and error handling

--- src/api/v1/controllers/AuthController.ts
+++ src/api/v1/controllers/AuthController.ts
@@ -2,13 +2,7 @@ import bcrypt from "bcrypt"
 import { Request, Response } from "express"
 import { sign } from "jsonwebtoken"
 import mongoose from "mongoose"
-import {
-    BadRequestError,
-    ConflictError,
-    InternalServerError,
-    NotFoundError,
-    UnauthorizedError
-} from "../../../middlewares/helpers/ApiErrors"
+import { BadRequestError, ConflictError, InternalServerError } from "../../../middlewares/helpers/ApiErrors"
 import User from "../models/User"
 
 export class AuthController {
@@ -17,14 +11,19 @@ export class AuthController {
         if (sessionData?.isLoggedIn) return res.status(302).redirect(`/dashboard/${sessionData?.data?.username}`)
 
         const { username, password } = req.body
-        if (!username || !password) throw new BadRequestError("Username and password are required.")
+        if (!username || !password)
+            return res.redirect(`/auth/login?error=${encodeURIComponent("Username and password are required.")}`)
 
         const user = await User.findOne({ username }).select("+password")
         const checkPassword = await user?.comparePassword(password)
-        if (!user || !checkPassword) throw new UnauthorizedError("Invalid username or password.")
+        if (!user || !checkPassword)
+            return res.redirect(`/auth/login?error=${encodeURIComponent("Invalid username or password.")}`)
 
         const secretKey = process.env.JWT_SECRET_KEY || "secret"
-        if (!secretKey) throw new NotFoundError("JWT secret key not found. Please contact the administrator.")
+        if (!secretKey)
+            return res.redirect(
+                `/auth/login?error=${encodeURIComponent("JWT secret key not found. \n Please contact the administrator.")}`
+            )
 
         const jwtToken = sign({ username: user.username }, secretKey, { expiresIn: "1d" })
 
@@ -35,15 +34,20 @@ export class AuthController {
             maxAge: 1 * 24 * 60 * 60 * 1000
         })
 
+        req.session.user = {
+            data: user,
+            isLoggedIn: true
+        }
+
         req.session.save((err) => {
             if (err) {
                 console.error(err)
-                throw new UnauthorizedError("Failed to save the session.")
+                return res.redirect(`/auth/login?error=${encodeURIComponent("Failed to save session.")}`)
             }
 
             res.locals.user = user
 
-            return res.status(200).json({ status: "success", message: "Logged in successfully" })
+            return res.status(200).redirect(`/dashboard/${user.username}`)
         })
     }
 
--- src/endpoints/auth.ts
+++ src/endpoints/auth.ts
@@ -6,8 +6,12 @@ routes.get("/register", (_req, res) =>
     res.status(200).render("pages/auth/register", { title: "Register", description: "Register a new account" })
 )
 
-routes.get("/login", (_req, res) =>
-    res.status(200).render("pages/auth/login", { title: "Login", description: "Login to your account" })
+routes.get("/login", (req, res) =>
+    res.status(200).render("pages/auth/login", {
+        title: "Login",
+        description: "Login to your account",
+        errorMessage: req.query.error
+    })
 )
 
 export default routes
