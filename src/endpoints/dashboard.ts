e788a0091e1962aea444e605f674af3aa7cc62ab
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-02-27 16:33:33 -0300

    feat: add latest posts to user dashboard, add better styles

--- src/endpoints/dashboard.ts
+++ src/endpoints/dashboard.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import Post from "../api/v1/models/Post"
 import User from "../api/v1/models/User"
 import authMiddleware from "../middlewares/AuthMiddleware"
 import { ForbiddenError, InternalServerError, NotFoundError } from "../middlewares/helpers/ApiErrors"
@@ -10,15 +11,26 @@ routes.get("/:username", authMiddleware, async (req, res) => {
     if (!username) throw new ForbiddenError("Username is missing. Try to log into your account again.")
 
     try {
-        const user = await User.findOne({ username })
+        const user = await User.findOne({ username }).select("-posts")
         if (!user) throw new NotFoundError("User not found. Try to log into your account again.")
 
-        const userHead = {
+        const latestPosts = await Post.find({ author: user._id }).sort({ createdAt: -1 }).limit(2)
+
+        const postsSummarized = latestPosts.map((post) => {
+            return {
+                ...post.toObject(),
+                content: post.content.substring(0, 100) + (post.content.length > 100 ? "..." : "")
+            }
+        })
+
+        const data = {
             title: `Dashboard - ${user.username}`,
-            description: `Welcome back, ${user.username}!`
+            description: `Welcome back, ${user.username}!`,
+            user,
+            latestPosts: postsSummarized
         }
 
-        return res.render("pages/users/dashboard", { ...userHead, user })
+        return res.render("pages/users/dashboard", data)
     } catch (error) {
         console.error(error)
         throw new InternalServerError("An error occurred while trying to log into your account.")
