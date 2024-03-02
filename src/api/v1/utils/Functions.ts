32be461c6780250dffb8e92db51f3b8e2b52bc9c
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-03-02 13:28:51 -0300

    feat: add parseDate function and update post view template

---
+++ src/api/v1/utils/Functions.ts
@@ -0,0 +1,9 @@
+export function parseDate(date: NativeDate | Date): string {
+    return new Date(date).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit"
+    })
+}
--- src/endpoints/posts.ts
+++ src/endpoints/posts.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response, Router } from "express"
 import { JSDOM } from "jsdom"
 import { marked } from "marked"
 import Post from "../api/v1/models/Post"
+import { parseDate } from "../api/v1/utils/Functions"
 import { NotFoundError } from "../middlewares/helpers/ApiErrors"
 
 const routes = Router({ mergeParams: true })
@@ -14,6 +15,9 @@ routes.get("/:postId/:postTitle", async (req: Request, res: Response, next: Next
     if (!postId || !postTitle) return next(new NotFoundError("Post not found."))
 
     const post = await Post.findById({ _id: postId })
+        .populate({ path: "author", select: "username displayName -_id" })
+        .populate({ path: "tags", select: "name -_id" })
+        .exec()
     if (!post) return next(new NotFoundError("Post not found."))
     if (post.title !== postTitle) return next(new NotFoundError("Post not found."))
 
@@ -28,7 +32,13 @@ routes.get("/:postId/:postTitle", async (req: Request, res: Response, next: Next
         title: post.title,
         description: post.content.substring(0, 100) + (post.content.length > 100 ? "..." : ""),
         loadPrismJS: true,
-        post
+        post: {
+            ...post.toObject(),
+            createdAt: parseDate(post.createdAt),
+            updatedAt: parseDate(post.updatedAt)
+        },
+        isUserLoggedIn: req.session.user?.isLoggedIn || false,
+        username: req.session.user?.isLoggedIn && req.session.user?.data ? req.session.user.data.username : null
     }
 
     res.status(200).render("pages/posts/post", data)
