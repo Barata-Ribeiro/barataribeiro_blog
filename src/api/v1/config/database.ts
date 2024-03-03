122e3fe35813731f8890cc16235a5927bee55f11
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-03-03 07:42:24 -0300

    fix: adjust database connection to include database name

--- src/api/v1/config/database.ts
+++ src/api/v1/config/database.ts
@@ -3,7 +3,8 @@ import mongoose from "mongoose"
 export const connectToDatabase = async () => {
     try {
         const URI = process.env.MONGODB_URI || "mongodb://localhost:27017/test"
-        await mongoose.connect(URI)
+        const dbName = process.env.MONGODB_DBNAME || "test"
+        await mongoose.connect(URI, { dbName })
         console.log("Connecting to the database...")
     } catch (error) {
         console.error("Failed to connect to the database: ", error)
