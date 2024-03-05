844b4bd2c32437ac3eaabc3bf24beb54536673da
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-03-05 08:20:38 -0300

    feat: add route to fetch posts with tags, paginated

--- src/endpoints/posts.ts
+++ src/endpoints/posts.ts
@@ -3,12 +3,84 @@ import { NextFunction, Request, Response, Router } from "express"
 import { JSDOM } from "jsdom"
 import { marked } from "marked"
 import Post from "../api/v1/models/Post"
+import Tag from "../api/v1/models/Tag"
 import { parseDate } from "../api/v1/utils/Functions"
 import { NotFoundError } from "../middlewares/helpers/ApiErrors"
 
 const routes = Router({ mergeParams: true })
 const window = new JSDOM("").window
 const purify = DOMPurify(window)
+
+routes.get("/", async (req: Request, res: Response) => {
+    let { tags, page = 1, limit = 10 } = req.query as { tags: string; page: string; limit: string }
+    page = Number(page)
+    limit = Number(limit)
+    const skip = (page - 1) * limit
+
+    let tagIds = [] as any[]
+    if (tags) {
+        const tagList = tags.split(",")
+        const foundTags = await Tag.find({ name: { $in: tagList } }).exec()
+        tagIds = foundTags.map((tag) => tag._id)
+    }
+    const query = tagIds.length > 0 ? { tags: { $in: tagIds } } : {}
+
+    const [posts, total] = await Promise.all([
+        Post.find(query)
+            .sort({ createdAt: -1 })
+            .populate({ path: "author", select: "username displayName -_id" })
+            .populate({ path: "tags", select: "name -_id" })
+            .skip(skip)
+            .limit(limit)
+            .exec(),
+        Post.countDocuments(query)
+    ])
+
+    if (posts.length === 0)
+        return res.status(200).render("pages/posts/posts", {
+            title: "Posts",
+            description:
+                "Here you will see the list of all the blog posts. Either all, or filtered through the posts' tags.",
+            data: {
+                posts: [],
+                total: 0,
+                perPage: limit,
+                currentPage: page,
+                nextPage: null,
+                prevPage: null
+            }
+        })
+
+    const serverOrigin = process.env.SERVER_ORIGIN || "http://localhost:3000"
+    const basePath = `${serverOrigin}/posts`
+
+    const baseParams = new URLSearchParams({
+        limit: limit.toString(),
+        ...(tagIds.length > 0 && { tags: tagIds.join(",") })
+    })
+    const nextPage = page * limit < total ? `${basePath}?${baseParams}&page=${page + 1}` : null
+    const prevPage = page > 1 ? `${basePath}?${baseParams}&page=${page - 1}` : null
+
+    const data = {
+        title: "Posts",
+        description:
+            "Here you will see the list of all the blog posts. Either all, or filtered through the posts' tags.",
+        data: {
+            posts: posts.map((post) => ({
+                ...post.toObject(),
+                createdAt: parseDate(post.createdAt),
+                updatedAt: parseDate(post.updatedAt)
+            })),
+            total,
+            perPage: limit,
+            currentPage: page,
+            nextPage,
+            prevPage
+        }
+    }
+
+    res.status(200).render("pages/posts/posts", data)
+})
 
 routes.get("/:postId/:postTitle", async (req: Request, res: Response, next: NextFunction) => {
     const { postId, postTitle } = req.params
