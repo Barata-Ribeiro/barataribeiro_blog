c9c6df53bdaa22dd7fb96585bfcc627f89b13a37
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-03-12 08:22:52 -0300

    feat: update post metadata and display creation and update timestamps

--- src/endpoints/posts.ts
+++ src/endpoints/posts.ts
@@ -108,12 +108,14 @@ routes.get("/:postId/:postSlug", async (req: Request, res: Response, next: NextF
     post.content = await purify.sanitize(post.content)
 
     const data = {
-        title: post.title,
-        description: post.summary + "...",
+        title: `${post.title} | Book of Shadows`,
+        description: post.summary + "... Read more on the blog.",
         keywords: post.tags.length > 0 ? post.tags.map((tag: any) => tag.name).join(", ") : "",
         loadPrismJS: true,
         post: {
             ...post.toObject(),
+            createdAtISO: parseDateToISO(post.createdAt),
+            updatedAtISO: parseDateToISO(post.updatedAt),
             createdAt: parseDate(post.createdAt),
             updatedAt: parseDate(post.updatedAt)
         },
