0264c53ac62f76724108309a1985793e7e7f6a1d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-03-16 07:21:24 -0300

    refactor: edit Tag model to add slug field and update name field validation

--- src/api/v1/models/Tag.ts
+++ src/api/v1/models/Tag.ts
@@ -5,7 +5,15 @@ const TagSchema = new Schema(
     {
         name: {
             type: String,
-            required: [true, "Name is required"],
+            trim: true,
+            required: [true, "is required"],
+            unique: true
+        },
+        slug: {
+            type: String,
+            lowercase: true,
+            required: true,
+            unique: true,
             index: true
         },
         posts: [{ type: Schema.Types.ObjectId, ref: "Post" }]
